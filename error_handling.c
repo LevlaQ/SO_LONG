d92bb185a831176b29058abc15af99bd79a7c254
Gülsüm Hümeyra Yıldız <gyildiz@k1m32s06.42istanbul.com.tr>
2025-03-03 14:52:49 +0300

    More error controls are added

--- error_handling.c
+++ error_handling.c
@@ -6,7 +6,7 @@
 /*   By: gyildiz <gyildiz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:54:38 by gyildiz           #+#    #+#             */
-/*   Updated: 2025/03/02 15:32:05 by gyildiz          ###   ########.fr       */
+/*   Updated: 2025/03/03 14:52:18 by gyildiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,24 @@
 int	error_main(int argc, char **argv)
 {
 	if (argc != 2)
-		return (write(2, "Error (Wrong argument count)\n", 29), 0);
+		return (write(2, "Error :Wrong argument count\n", 28), 0);
 	if (!check_filename(argv[1]))
-		return (write(2, "Error (Wrong file extension)\n", 29), 0);
+		return (write(2, "Error :Wrong file extension\n", 28), 0);
 	if (!(verify_file(argv[1])))
-		return (write(2,"Error (No file or no proper permissions)\n", 41), 0);
-	if (!(file_to_string(argv[1])))
-		return (write(2,"Error (Malloc failure or empty file)\n", 37), 0);
+		return (write(2,"Error :No file or no proper permissions\n", 40), 0);
+	if (!(check_map_chars(argv[1])))
+		return (write(2,"Error :Invalid chars in map / malloc fail\n", 42), 0);
+	if (!(check_map_elements(argv[1])))
+		return (write(2,"Error :Must have 1P1E & C≥1 / malloc fail\n", 42), 0);
 	return (1);
 }
 
+//Leak ihtimali!!!
 int	check_filename(char *s)
 {
 	int	i;
-
+	char	*no_exten;
+	
 	i = ft_strlen(s); //Döndürdüğü sayıyı direkt indekse alsam \0'dan başlarım olmaz
 	if (s[i - 1] != 'r')
 		return (0);
@@ -38,7 +42,10 @@ int	check_filename(char *s)
 		return (0);
 	if (s[i - 4] != '.')
 		return (0);
-	return (1);
+	no_exten = ft_substr(s, 0, i - 4); //No exten malloc ile açıldı
+	if (ft_strchr(no_exten, '.') != NULL);
+		return (free(no_exten), 0);
+	return (free(no_exten), 1);
 }
 
 //Eğer bu fonksiyon doğru ise hemen karakter kontrollerine girsin
@@ -76,4 +83,70 @@ char	*file_to_string(char *s)
 		new = get_next_line(fd);
 	}
 	return (joined);
+}
+
+/*
+	Haritanın içerisinde 1, 0, P, E, C, \n harici 
+*/
+int	check_map_chars(char *s)
+{
+	int		i;
+	char	*joined;
+	
+	i = 0;
+	joined = file_to_string(s);
+	while (joined[i])
+	{
+		if (joined[i] != '1' && joined[i] != '0' && joined[i] != 'C' &&
+			joined[i] != 'P' && joined[i] != 'E' && joined[i] != '\n')
+			return (free(joined), 0);
+		i ++;
+	}
+	return (free(joined), 1);
+}
+/*
+	Haritanın içerisinde hiç C yoksa
+	Haritanın içerisinde 1 den fazla P ve E varsa => 0
+	Herşey yolunda ise 0 => 1
+*/
+int	check_map_elements(char *s)
+{
+	int		i;
+	int		count_p;
+	int		count_e;
+	char	*joined;
+
+	joined = file_to_string(s);
+	if(ft_strchr(joined, 'C') == NULL)
+		return(free(joined), 0);
+	i = 0;
+	count_p = 0;
+	count_e = 0;
+	while(joined[i])
+	{
+		if(ft_strchr(joined, 'P') != NULL)
+			count_p++;
+		if(ft_strchr(joined, 'E') != NULL)
+			count_e++;
+		i++;
+	}
+	if (count_p != 1)
+		return (free(joined), 0);
+	if (count_e != 1)
+		return (free(joined), 0);
+	return (free(joined), 1);
+}
+
+/*
+	Argüman satırından dosya ismini alığ open() ile açacak
+*/
+int		validate_map_shape(char *s)
+{
+	int	fd;
+}
+
+//GNL ile haritayı 2D array'e al
+char	**file_to_string_matrix(int fd)
+{
+	
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: gyildiz <gyildiz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:58:11 by gyildiz           #+#    #+#             */
-/*   Updated: 2025/03/02 15:11:03 by gyildiz          ###   ########.fr       */
+/*   Updated: 2025/03/03 14:20:45 by gyildiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,11 @@ typedef	struct s_map
 int		error_main(int argc, char **argv);
 int		check_filename(char *s);
 int		verify_file(char *s);
-int		check_valid_chars(int fd);
 char	*ft_strdup_modified(char *s1);
 char	*ft_strjoin_modified(char *s1, char *s2);
 size_t	ft_strlen_modified(char *s);
-char	*file_to_string(int fd);
+char	*file_to_string(char *s);
+int		check_map_chars(char *s);
+int		check_map_elements(char *s);
 
 #endif
